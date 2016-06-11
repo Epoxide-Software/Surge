94e0eaefa980fa9378ee77a860c6c8ae7ccdb9a7
Tyler Hancock <darklime@live.ca>
2016-06-11 07:25:06 -0600

    Removed the event. Self contained classes for features.

--- src/main/java/net/epoxide/surge/client/ProxyClient.java
+++ src/main/java/net/epoxide/surge/client/ProxyClient.java
@@ -3,14 +3,11 @@ package net.epoxide.surge.client;
 import net.epoxide.surge.Surge;
 import net.epoxide.surge.common.ProxyCommon;
 import net.epoxide.surge.features.Feature;
-import net.minecraftforge.common.MinecraftForge;
 
 public class ProxyClient extends ProxyCommon {
     
     @Override
     public void onPreInit () {
-        
-        MinecraftForge.EVENT_BUS.register(new ClientEventHandler());
         
         Surge.features.forEach(Feature::setupRendering);
     }
