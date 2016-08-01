12b59b9975002335818b51fc0094e11589bbc7bd
Tyler Hancock <darklime@live.ca>
2016-07-31 21:53:19 -0600

    Fixed mod id

--- src/main/java/net/epoxide/surge/libs/Constants.java
+++ src/main/java/net/epoxide/surge/libs/Constants.java
@@ -7,7 +7,7 @@ import org.apache.logging.log4j.Logger;
 
 public class Constants {
     
-    public static final String MOD_ID = "Surge";
+    public static final String MOD_ID = "surge";
     public static final String MOD_NAME = "Surge";
     public static final String VERSION_NUMBER = "0.0.0.0";
     public static final String CLIENT_PROXY_CLASS = "net.epoxide.surge.client.ProxyClient";
