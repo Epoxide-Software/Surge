18415735b14bb0c2c39b561d7434ac151afa0c3a
Tyler Hancock <darklime@live.ca>
2016-08-01 03:48:53 -0600

    Split stuff into client and common, fixes server issues.

--- src/main/java/net/epoxide/surge/features/FeatureManager.java
+++ src/main/java/net/epoxide/surge/features/FeatureManager.java
@@ -9,6 +9,8 @@ import net.epoxide.surge.features.rendering.FeatureGroupRenderCulling;
 import net.epoxide.surge.features.rendering.FeatureHidePlayer;
 import net.epoxide.surge.features.rendering.FeatureHideUnseenEntities;
 import net.epoxide.surge.handler.ConfigurationHandler;
+import net.minecraftforge.fml.relauncher.FMLLaunchHandler;
+import net.minecraftforge.fml.relauncher.Side;
 
 public class FeatureManager {
     
@@ -29,10 +31,14 @@ public class FeatureManager {
      */
     public static void initFeatures () {
         
-        featureHidePlayers = registerFeature(new FeatureHidePlayer(), "Hide Players", "Command to disable the rendering of other players on the client.");
-        featureRedstoneFix = registerFeature(new FeatureRedstoneFix(), "Redstone Toggle Fix", "Fixes a memory leak with toggle state of redstone torches. MC-101233");
-        featureGroupRenderCulling = registerFeature(new FeatureGroupRenderCulling(), "Group Render Culling", "Cuts down on the amount of entities rendered, when they are bunched together.");
-        featureHideUnseenEntities = registerFeature(new FeatureHideUnseenEntities(), "Hide Unseen Entities", "Prevents the rendering of entities that are not in view of the camera.");
+        if (FMLLaunchHandler.side() == Side.CLIENT) {
+            
+            featureHideUnseenEntities = registerFeature(new FeatureHideUnseenEntities(), "Hide Unseen Entities", "Prevents the rendering of entities that are not in view of the camera.");
+            featureGroupRenderCulling = registerFeature(new FeatureGroupRenderCulling(), "Group Render Culling", "Cuts down on the amount of entities rendered, when they are bunched together.");
+            featureHidePlayers = registerFeature(new FeatureHidePlayer(), "Hide Players", "Command to disable the rendering of other players on the client.");
+            featureRedstoneFix = registerFeature(new FeatureRedstoneFix(), "Redstone Toggle Fix", "Fixes a memory leak with toggle state of redstone torches. MC-101233");
+        }
+        
         featureLoadTimes = registerFeature(new FeatureLoadTimes(), "Load Time Analysis", "Records the load time of all mods being loaded.");
     }
     
