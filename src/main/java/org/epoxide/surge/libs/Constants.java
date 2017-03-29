9207a2abf8d1504e287f6fee045ee5ad6b289fd9
spikespaz <spikespaz@outlook.com>
2017-03-29 15:04:16 -0700

    Fix mcmod.info issues

--- src/main/java/org/epoxide/surge/libs/Constants.java
+++ src/main/java/org/epoxide/surge/libs/Constants.java
@@ -9,7 +9,7 @@ public class Constants {
 
     public static final String MOD_ID = "surge";
     public static final String MOD_NAME = "Surge";
-    public static final String VERSION = "1.2.0.1";
+    public static final String VERSION = "1.2.0.2";
     public static final String CLIENT_PROXY_CLASS = "org.epoxide.surge.client.ProxyClient";
     public static final String SERVER_PROXY_CLASS = "org.epoxide.surge.common.ProxyCommon";
     public static final String DEPENDENCIES = "";
